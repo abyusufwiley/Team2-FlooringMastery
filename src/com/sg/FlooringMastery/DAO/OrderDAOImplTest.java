ceed5cf638a7d48fe83ec2ff15dcad17704e5b45
thouf99 <thouf616@gmail.com>
2024-03-26 16:15:07 +0000

    Struggling
    Struggling with the remove test

--- src/com/sg/FlooringMastery/DAO/OrderDAOImplTest.java
+++ src/com/sg/FlooringMastery/DAO/OrderDAOImplTest.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -157,41 +158,81 @@ class OrderDAOImplTest {
 //
 //        // ACT
 //        OrderDTO addedOrder = testDAO.addOrder(order);
-//        testDAO.removeOrder(orderId, addedOrder.getDate());
+//        testDAO.removeOrder(orderId, order.getDate());
 //
 //        // ASSERT
-//        OrderDTO removedOrder = testDAO.getOrder(orderId, addedOrder.getDate());
-//        System.out.println(removedOrder);
+//        OrderDTO removedOrder = testDAO.getOrder(orderId, order.getDate());
 //        assertNull(removedOrder, "The order should be removed.");
 //    }
 
     @Test
-    @DisplayName("Test removeOrder")
     public void testRemoveOrder() throws Exception {
-        // ARRANGE
-        int orderId = 1;
-        OrderDTO order = new OrderDTO(orderId);
-        order.setDate(LocalDate.parse("2024-03-26"));
-        order.setCustomerName("Ada Lovelace");
-        order.setState("CA");
-        order.setTaxRate(new BigDecimal("25.00"));
-        order.setProductType("Tile");
-        order.setArea(new BigDecimal("249.00"));
-        order.setCostPerSquareFoot(new BigDecimal("3.50"));
-        order.setLaborCostPerSquareFoot(new BigDecimal("4.15"));
-        order.setMaterialCost(new BigDecimal("871.50"));
-        order.setLaborCost(new BigDecimal("1033.35"));
-        order.setTax(new BigDecimal("476.21"));
-        order.setTotal(new BigDecimal("2381.06"));
+        // Create two new orders
+        OrderDTO firstOrder = new OrderDTO(1);
+        firstOrder.setDate(LocalDate.parse("2024-03-26"));
+        firstOrder.setCustomerName("Ada Lovelace");
+        firstOrder.setState("CA");
+        firstOrder.setTaxRate(new BigDecimal("25.00"));
+        firstOrder.setProductType("Tile");
+        firstOrder.setArea(new BigDecimal("249.00"));
+        firstOrder.setCostPerSquareFoot(new BigDecimal("3.50"));
+        firstOrder.setLaborCostPerSquareFoot(new BigDecimal("4.15"));
+        firstOrder.setMaterialCost(new BigDecimal("871.50"));
+        firstOrder.setLaborCost(new BigDecimal("1033.35"));
+        firstOrder.setTax(new BigDecimal("476.21"));
+        firstOrder.setTotal(new BigDecimal("2381.06"));
 
-        // ACT
-        OrderDTO addedOrder = testDAO.addOrder(order);
-        OrderDTO removedOrder = testDAO.removeOrder(orderId, addedOrder.getDate());
+        OrderDTO secondOrder = new OrderDTO(2);
+        secondOrder.setCustomerName("Grace Hopper");
+        secondOrder.setDate(LocalDate.parse("2024-03-26"));
+        secondOrder.setState("WA");
+        secondOrder.setTaxRate(new BigDecimal("9.25"));
+        secondOrder.setProductType("Carpet");
+        secondOrder.setArea(new BigDecimal("243.00"));
+        secondOrder.setCostPerSquareFoot(new BigDecimal("2.25"));
+        secondOrder.setLaborCostPerSquareFoot(new BigDecimal("2.10"));
+        secondOrder.setMaterialCost(new BigDecimal("546.75"));
+        secondOrder.setLaborCost(new BigDecimal("510.30"));
+        secondOrder.setTax(new BigDecimal("50.54"));
+        secondOrder.setTotal(new BigDecimal("1107.59"));
 
-        // ASSERT
-        assertEquals(order, addedOrder, "The order should be added.");
-        assertEquals(order, removedOrder, "The order should be removed.");
-        assertNull(testDAO.getOrder(orderId,addedOrder.getDate()), "The order should be null.");
+        // Add both to the DAO
+        testDAO.addOrder(firstOrder);
+        testDAO.addOrder(secondOrder);
+
+        // remove the first order - Ada's
+        OrderDTO removedOrder = testDAO.removeOrder(firstOrder.getOrderNumber(), firstOrder.getDate());
+
+        // Check that the correct object was removed.
+        assertEquals(removedOrder, firstOrder, "The removed order should be Ada's.");
+
+        // Get all the orders
+        List<OrderDTO> allOrders = testDAO.getAllOrders();
+
+        // First check the general contents of the list
+        assertNotNull(allOrders, "All orders list should be not null.");
+        assertEquals(1, allOrders.size(), "All orders should only have 1 order.");
+
+        // Then the specifics
+        assertFalse(allOrders.contains(firstOrder), "All orders should NOT include Ada's.");
+        assertTrue(allOrders.contains(secondOrder), "All orders should include Charles's.");
+
+        // Remove the second order
+        removedOrder = testDAO.removeOrder(secondOrder.getOrderNumber(), secondOrder.getDate());
+        // Check that the correct object was removed.
+        assertEquals(removedOrder, secondOrder, "The removed order should be Charles's.");
+
+        // retrieve all the orders again, and check the list.
+        allOrders = testDAO.getAllOrders();
+
+        // Check the contents of the list - it should be empty
+        assertTrue(allOrders.isEmpty(), "The retrieved list of orders should be empty.");
+
+        // Try to 'get' both orders by their old id - they should be null!
+        OrderDTO retrievedOrder = testDAO.getOrder(firstOrder.getOrderNumber(), firstOrder.getDate());
+        assertNull(retrievedOrder, "Ada's order was removed, should be null.");
+
+        retrievedOrder = testDAO.getOrder(secondOrder.getOrderNumber(), secondOrder.getDate());
+        assertNull(retrievedOrder, "Charles's order was removed, should be null.");
     }
-
 }
